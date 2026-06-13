3570407785b889713eb39d7d32ac4d6d9ce07111
Lucas <lucasluz701@gmail.com>
2026-06-13 14:34:43 -0300

    Navbar mobile and desktop with style and animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,31 @@
 /*=============== SHOW & CLOSE MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/* Show menu */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
 
+/* Hide menu */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
 /*=============== REMOVE MOBILE MENU ===============*/
 
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+   const navMenu = document.getElementById('nav-menu')
+   // When we click on each nav__link, we remove the show-menu class
+   navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE HEADER STYLES ===============*/
 
