b3ec942751fd4436c96c42412b39457acffe7557
Lucas <lucasluz701@gmail.com>
2026-06-13 17:09:03 -0300

    change header styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,13 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE HEADER STYLES ===============*/
 
+const scrollHeader = () =>{
+   const header = document.getElementById('header')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+   this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                      : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER WORKS ===============*/
 
